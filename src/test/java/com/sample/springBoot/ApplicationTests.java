1ef84487214ed15e93a90140338e9d5032983f02
Oluwatobi <tboydv1@gmail.com>
2020-03-21 15:27:58 +0100

    Main application initial commmit

---
+++ src/test/java/com/sample/springBoot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
